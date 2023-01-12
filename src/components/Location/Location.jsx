433f8f3b106a10f2abddb259fa752fb0921701d8
Anthony Chanty <anthony.chanty@hotmail.com>
2023-01-12 23:23:34 +0100

    remove Location comments

--- src/components/Location/Location.jsx
+++ src/components/Location/Location.jsx
@@ -1,15 +1,11 @@
 import './Location.css'
-// import { NavLink } from 'react-router-dom'
 
 function Location({ location }) {
    return (
-      //   <div className="header__area-wrapper">
       <div className="location">
-         {/* <p className="header__area"> */}
          <p className="location__area">
             <i className="fa-solid fa-location-dot"></i>
             {location}
-            {/* Paris, Belleville */}
          </p>
       </div>
    )

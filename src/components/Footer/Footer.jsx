9827792cd08513c4da5027ac471d8766894b1585
Anthony Chanty <anthony.chanty@hotmail.com>
2023-01-12 23:12:31 +0100

    refactor Footer

--- src/components/Footer/Footer.jsx
+++ src/components/Footer/Footer.jsx
@@ -2,30 +2,44 @@ import './Footer.css'
 import logo from '../../assets/logos/ohmyfood@2x.svg'
 import { NavLink } from 'react-router-dom'
 
+const links = [
+   {
+      icon: 'fa-utensils',
+      text: 'Proposer un restaurant',
+      path: '/',
+   },
+   {
+      icon: 'fa-handshake-angle',
+      text: 'Devenir partenaire',
+      path: '/',
+   },
+   {
+      text: 'Mentions légales',
+      path: '/',
+   },
+   {
+      text: 'Contact',
+      path: '/',
+   },
+]
+
 function Footer() {
    return (
       <footer className="footer">
          <div className="footer__content">
             <img src={logo} alt="logo ohmyfood" />
             <ul>
-               <li>
-                  <NavLink to="/">
-                     <i className="fa-solid fa-utensils"></i>Proposer un
-                     restaurant
-                  </NavLink>
-               </li>
-               <li>
-                  <NavLink to="/">
-                     <i className="fa-solid fa-handshake-angle"></i>Devenir
-                     partenaire
-                  </NavLink>
-               </li>
-               <li>
-                  <NavLink to="/">Mentions légales </NavLink>
-               </li>
-               <li>
-                  <NavLink to="/">Contact </NavLink>
-               </li>
+               {/* parcourt le tableau "links" pour créer un élément pour chaque entrée présente */}
+               {links.map((link, index) => (
+                  <li key={index}>
+                     <NavLink to={link.path}>
+                        {link.icon && ( // injecte ce code si l'objet du tableau contient une valeur "icon"
+                           <i className={`fa-solid ${link.icon}`}></i>
+                        )}
+                        {link.text}
+                     </NavLink>
+                  </li>
+               ))}
             </ul>
          </div>
       </footer>

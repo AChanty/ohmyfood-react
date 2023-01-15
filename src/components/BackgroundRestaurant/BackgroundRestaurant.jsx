09af323aec2a7e45ee38214ee292186045e67d3d
Anthony Chanty <anthony.chanty@hotmail.com>
2023-01-15 02:06:35 +0100

    fix right image for first restaurant

--- src/components/BackgroundRestaurant/BackgroundRestaurant.jsx
+++ src/components/BackgroundRestaurant/BackgroundRestaurant.jsx
@@ -5,7 +5,8 @@ function BackgroundRestaurant({ source }) {
       <div
          className="restaurant__background"
          style={{
-            backgroundImage: `url(${source})`,
+            // backgroundImage: `url(${source})`,
+            backgroundImage: `url(${source}`,
          }}
       ></div>
    )
